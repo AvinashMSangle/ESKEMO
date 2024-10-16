7ead7b610530ad443c769caa6e5dc3a88fa5fa8e
AvinashMSangle <avinashMS.3119@proton.me>
2024-10-16 07:00:15 +0000

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,66 @@
+// MARK: Imports
 import Option from "./components/Option";
 
-// https://dog.ceo/api/breeds/list/all
 // https://dog.ceo/api/breed/affenpinscher/images/random
+// https://dog.ceo/api/breeds/list/all
 
 const BASE_URL = `https://dog.ceo/api/`;
 
-const imageEl = document.querySelector("img");
+// === MARK: DOM Selection
 const breedListEl = document.querySelector("#data-breed-list");
+const imageEl = document.querySelector("img");
 
-//  async function getImage(){
-//     const res = await fetch(
-//         `https://dog.ceo/api/breed/affenpinscher/images/random`
-//     );
-//     const data = await res.json();
-//     console.log(data.message);
-
-//     imageEl.src = data.message;
-
-// }
-
-// getImage();
-
-// === Fetch Functions
-
-function getDogList() {
- return fetch(`${BASE_URL}breeds/list/all`)
-    .then((res) => res.json())
-    .then((data) => data.message)
-    .catch((err) => console.error("error agayi", err));
+// === MARK: Fetch
+async function getDogsList() {
+  try {
+    
+    const res = await fetch(`${BASE_URL}breeds/list/all`);
+    const data = await res.json();
+    return data.message;
+  } catch (err) {
+    console.error("Error occured", err);
+  }
 }
 
-// TODO: Implement this
-function getDogImage(breed) {
-    fetch(`${BASE_URL}breed/${breed}/image/random`)
+// Fetch a single dog breed image
+async function getDogImage(breed) {
+  try {
+    const res = await fetch(`${BASE_URL}breed/${breed}/images/random`);
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    return console.error(error);
+  }
 }
 
-// === MARK : Render
-function renderSelect() {
-    getDogList().then((breedList) =>{
-        for (let breed in breedList){
-            breedListEl.appendChild(Option(breed));
-            // const option = document.createElement("option");
-            // option.textContent = "breed";
-            // option.value = "breed";
-            // breedListEl.appendChild(option);
-        }
-    })
+// === MARK: Render
+async function renderSelect() {
+  const dogsList = await getDogsList();
 
+  const fragment = document.createDocumentFragment();
+
+  Object.keys(dogsList).forEach((dogName) => {
+    fragment.appendChild(Option(dogName));
+  });
+
+  breedListEl.append(fragment);
 }
-renderSelect();
 
-function renderImage() {}
+async function renderImage(breed) {
+  imageEl.src = "loading.gif";
+
+  const dogImage = await getDogImage(breed);
+  imageEl.src = dogImage;
+  imageEl.alt = breed;
+}
+
+// === MARK:  Events
+breedListEl.addEventListener("change", async (e) => {
+  const currentValue = e.target.value;
+  renderImage(currentValue);
+});
+
+// === Render on inital load
+document.addEventListener("DOMContentLoaded", () => {
+  renderSelect();
+});
